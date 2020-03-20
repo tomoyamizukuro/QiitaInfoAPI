6ad6e3a695bcf262da812beff22e4402db1fa511
tomoyamizukuro <salvatoleyohane@gmail.com>
2020-03-20 15:13:51 +0900

    api からデータを取得処理実装

--- src/js/main.js
+++ src/js/main.js
@@ -9,9 +9,29 @@ var app = new Vue({
 
   },
   created: function() {
-
+    this.keyword = 'JavaScript'
+    this.getAnswer()
   },
-  method: {
-
+  methods: {
+    getAnswer: function(){
+      if(this.keyword === ''){
+        this.item = null
+        return
+      }
+      this.message = 'Loading...'
+      var vm = this
+      var params = { page: 1, per_page: 20, query: this.keyword }
+      axios.get('https://qiita.com/api/v2/items', {params})
+      .then(function(response){
+        console.log(response)
+        vm.items = response.data
+      })
+      .catch(function(error){
+        vm.message = 'Error' + error
+      })
+      .finally(function(){
+        vm.message = ''
+      })
+    }
   }
 })
